d85d2634495f668bd055ef26cfd0af1cdb3de97a
Your Name <amitgaur@gmail.com>
2012-05-23 08:42:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbMongo::Application.config.secret_token = 'c0b9d0d7d39cc1ca562c8e18ea8f281182aaa3423a07b441691c265b66c769d0b3e5da789e9dde19ef32de8a7e06020ffc624e3d9e6c543bb4d40e412c82ee49'
